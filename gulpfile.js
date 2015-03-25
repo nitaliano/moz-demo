ef3ccf7cd8bc7f3ae8b7babf400f1adf3ba2f70f
unboud <ni6@njit.edu>
2015-03-24 20:09:54 -0400

    Exclude precompiled templates from lint

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,12 @@ var jshint = require('gulp-jshint');
 var stylish = require('jshint-stylish');
 
 gulp.task('lint', function () {
-	return gulp.src(['./models/**/*.js', './controllers/**/*.js', './public/js/**/*.js'])
+	return gulp.src([
+		'./models/**/*.js', 
+		'./controllers/**/*.js', 
+		'./public/js/**/*.js', 
+		'!./public/js/templates.js'
+	])
 	.pipe(jshint())
 	.pipe(jshint.reporter(stylish));
 });
