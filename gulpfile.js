5f307229034d339daae6deb4fa820e9e63a68824
unboud <ni6@njit.edu>
2015-03-24 20:15:08 -0400

    Added task to convert tabs to spaces

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,23 @@
 var gulp   = require('gulp');
 var jshint = require('gulp-jshint');
 var stylish = require('jshint-stylish');
+var soften = require('gulp-soften');
+
+var dirs = [
+  './models/**/*.js', 
+  './controllers/**/*.js', 
+  './public/js/**/*.js', 
+  '!./public/js/templates.js'
+];
 
 gulp.task('lint', function () {
-	return gulp.src([
-		'./models/**/*.js', 
-		'./controllers/**/*.js', 
-		'./public/js/**/*.js', 
-		'!./public/js/templates.js'
-	])
-	.pipe(jshint())
-	.pipe(jshint.reporter(stylish));
+  return gulp.src(dirs)
+    .pipe(jshint())
+    .pipe(jshint.reporter(stylish));
 });
+ 
+gulp.task('soften', function () {
+  gulp.src(dirs)
+    .pipe(soften(2))
+    .pipe(gulp.dest('./'));
+})
