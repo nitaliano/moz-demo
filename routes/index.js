c09f15f974283b8ebca799b2a0fa94571ac50f25
unboud <ni6@njit.edu>
2015-03-24 20:15:20 -0400

    Converted tabs to spaces

--- routes/index.js
+++ routes/index.js
@@ -1,53 +1,53 @@
 var fs = require('fs');
 
 module.exports = function (app) {
-	// bootstrap controllers
-	initControllers(app);
+  // bootstrap controllers
+  initControllers(app);
 
-	// catch 404 and forward to error handler
-	app.use(function(req, res, next) {
-	  var err = new Error('Not Found');
-	  err.status = 404;
-	  next(err);
-	});
+  // catch 404 and forward to error handler
+  app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+  });
 
-	// development error handler
-	// will print stacktrace
-	if (app.get('env') === 'development') {
-	  app.use(function(err, req, res, next) {
-	    res.status(err.status || 500);
-	    res.render('error', {
-	      message: err.message,
-	      error: err
-	    });
-	  });
-	}
+  // development error handler
+  // will print stacktrace
+  if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+      res.status(err.status || 500);
+      res.render('error', {
+        message: err.message,
+        error: err
+      });
+    });
+  }
 
-	// production error handler
-	// no stacktraces leaked to user
-	app.use(function(err, req, res, next) {
-	  res.status(err.status || 500);
-	  res.render('error', {
-	    message: err.message,
-	    error: {}
-	  });
-	});
+  // production error handler
+  // no stacktraces leaked to user
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: {}
+    });
+  });
 
 };
 
 
 function initControllers(app) {
-	var i, controller, Controller, files = fs.readdirSync('./controllers');
+  var i, controller, Controller, files = fs.readdirSync('./controllers');
 
-	if (!files.length) {
-		throw new Error('Controllers could not be found!!!!!!');
-	}
+  if (!files.length) {
+    throw new Error('Controllers could not be found!!!!!!');
+  }
 
-	for (i = 0; i < files.length; i++) {
-		if (files[i] !== 'base.js') {
-			Controller = require('../controllers/' + files[i]);
-			controller = new Controller(app);
-			controller.handle();			
-		}
-	}
+  for (i = 0; i < files.length; i++) {
+    if (files[i] !== 'base.js') {
+      Controller = require('../controllers/' + files[i]);
+      controller = new Controller(app);
+      controller.handle();      
+    }
+  }
 }
